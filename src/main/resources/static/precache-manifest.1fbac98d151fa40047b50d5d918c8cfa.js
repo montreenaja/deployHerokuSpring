778ac17bbce84ff63dfdb903c2bb485e5be1ebed
premanan <montree_iq205178@hotmail.com>
2022-04-07 01:06:45 +0700

    fix bug

---
+++ src/main/resources/static/precache-manifest.1fbac98d151fa40047b50d5d918c8cfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a82fe3d9fb37d002eb2",
+    "url": "/static/css/main.a3261e63.chunk.css"
+  },
+  {
+    "revision": "0a82fe3d9fb37d002eb2",
+    "url": "/static/js/main.0a82fe3d.chunk.js"
+  },
+  {
+    "revision": "f76d16948cf577105489",
+    "url": "/static/css/1.a7d384f1.chunk.css"
+  },
+  {
+    "revision": "f76d16948cf577105489",
+    "url": "/static/js/1.f76d1694.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "514de7eece4f5d87177b2cb102c71a4c",
+    "url": "/index.html"
+  }
+];
